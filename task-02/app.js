f7a974cf3a0eddefb275e01b31a98b1f2be6c3c5
Nisarga Murthy <nisargamurthybp@gmail.com>
2026-02-18 15:44:23 +0530

    Fixed Jenkinsfile for Linux agent

--- task-02/app.js
+++ task-02/app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Jenkins pipeline working");
+  res.send("Jenkins CI/CD working");
 });
 
 app.listen(3000, () => {
-  console.log("Server running on port 3000");
+  console.log("Server running");
 });
